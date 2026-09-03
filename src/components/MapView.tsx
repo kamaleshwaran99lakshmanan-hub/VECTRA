d411d336d1e1478592c173adb9ee6c9fecc363e7
Kamaleshwaran <kamaleshwaran99lakshmanan@gmail.com>
2026-09-04 01:19:23 +0530

    Build VECTRA operations dashboard

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -7,7 +7,25 @@ import {
   Popup,
   useMap,
 } from "react-leaflet";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
 import "leaflet/dist/leaflet.css";
+
+const DefaultIcon = L.icon({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface RouteCoordinate {
   lat: number;
